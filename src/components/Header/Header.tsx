e3457bff44e1001c0726fa82f92ae86b513b3ef4
Chaomans <chaomans@gmail.com>
2024-08-12 09:26:29 +0200

    Header completed

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,30 +1,44 @@
 import { NavLink } from "react-router-dom";
+import styles from "./Header.module.scss";
 
 type HeaderProps = {
-    isConnected?: boolean;
+  isConnected?: boolean;
 };
 
 const Header = ({ isConnected = false }: HeaderProps) => {
-    return (
-        <div className="header">
-            <NavLink className="logo" to={"/"}>
-                <img src="" alt="argentbank logo" />
-            </NavLink>
-            <div className="user">
-                <div className="userPicture">
-                    <img src="" alt="user icon" />
-                    {isConnected ? "User name" : "Sign In"}
-                </div>
-
-                {isConnected && (
-                    <div className="logout">
-                        <img src="" alt="logout icon" />
-                        <p>Sign Out</p>
-                    </div>
-                )}
-            </div>
+  return (
+    <nav className={styles.mainNav}>
+      <NavLink className={styles.mainNavLogo} to={"/"}>
+        <img
+          src="/assets/argentBankLogo.png"
+          alt="argentbank logo"
+          className={styles.mainNavLogoImage}
+        />
+        <h1 className={styles.srOnly}>Argent Bank</h1>
+      </NavLink>
+      <div className={styles.mainNavUser}>
+        <div className={styles.user}>
+          <img
+            src="/assets/user-circle.svg"
+            alt="user icon"
+            className={styles.userCircle}
+          />
+          <p>{isConnected ? " User name" : "Sign In"}</p>
         </div>
-    );
+
+        {isConnected && (
+          <div className={styles.logout}>
+            <img
+              src="/assets/sign-out.svg"
+              alt="logout icon"
+              className={styles.logoutIcon}
+            />
+            <p>Sign Out</p>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
