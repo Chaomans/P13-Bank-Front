e5101bb624ac87665e558ad68586c45c417cdc1f
Chaomans <chaomans@gmail.com>
2024-08-16 13:02:35 +0200

    signin

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,5 +1,41 @@
+import styles from "./Login.module.scss";
+
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <section className={styles.signInContent}>
+      <div className={styles.signInBox}>
+        <div className={styles.iconDiv}>
+          <img
+            className={styles.signInIcon}
+            src="/assets/user-circle.svg"
+            alt="user icon"
+          />
+        </div>
+        <h2 className={styles.title}>Sign In</h2>
+        <form className={styles.form}>
+          <div className={styles.inputWrapper}>
+            <label className={styles.inputLabel} htmlFor="username">
+              Username
+            </label>
+            <input className={styles.input} type="text" id="username" />
+          </div>
+          <div className={styles.inputWrapper}>
+            <label className={styles.inputLabel} htmlFor="password">
+              Password
+            </label>
+            <input className={styles.input} type="password" id="password" />
+          </div>
+          <div className={styles.inputRemember}>
+            <input className={styles.input} type="checkbox" id="remember-me" />
+            <label className={styles.inputLabel} htmlFor="remember-me">
+              Remember me
+            </label>
+          </div>
+          <button className={styles.signInButton}>Sign In</button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Login;
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -3,3 +3,24 @@ export type Feature = {
   title: string;
   text: string;
 };
+
+export type Login = {
+  email: string;
+  password: string;
+}
+
+export type UserProfile = {
+  firstName: string;
+  lastname: string;
+}
+
+export type User = UserProfile & Login;
+
+export type ApiResponse = {
+  status: number;
+  message: string;
+  body: {
+    id: string,
+    email: string
+  }
+}
